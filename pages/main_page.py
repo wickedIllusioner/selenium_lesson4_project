56be1907d618aafbc906a05b8c080e5b67accab3
Artyom Veselov <xxxvanyalive228xxx@gmail.com>
2023-11-19 23:03:51 +0800

    apply minor changes

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,5 @@
 from .base_page import BasePage
-from .login_page import LoginPage
 from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
--- test_login_page.py
+++ test_login_page.py
@@ -1,4 +1,3 @@
-from .pages.locators import MainPageLocators
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
