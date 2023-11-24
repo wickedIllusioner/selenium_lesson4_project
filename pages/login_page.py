aed79ff816b588e939e68836193e601754db9ab4
Artyom Veselov <xxxvanyalive228xxx@gmail.com>
2023-11-24 23:59:11 +0800

    add test scenarios for registered users

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,10 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from random_word import RandomWords
+
+import secrets
+import string
+import random
 # from selenium.webdriver.support.wait import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 
@@ -18,3 +23,28 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Login form is not found on the page'
+    
+
+    def register_new_user(self, email, pswd):
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).send_keys(pswd)
+        self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD).send_keys(pswd)
+
+        registration_submit_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        registration_submit_button.click()
+
+
+    def generate_email_and_password(self):
+        r = RandomWords()
+        email = r.get_random_word() + '@hotmail.com'
+        
+        letters = string.ascii_letters
+        digits = string.digits
+        pwd_length = random.randrange(10, 25)
+        alphabet = letters + digits
+
+        password = ''
+        for _ in range(pwd_length):
+            password += ''.join(secrets.choice(alphabet))
+
+        return email, password
